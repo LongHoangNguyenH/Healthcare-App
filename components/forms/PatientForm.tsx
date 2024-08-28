d37d5f53022196ec876d0df6c02131fb1a2a810b
LongHoangNguyenH <longnh.uit@gmail.com>
2024-08-28 19:56:32 +0700

    Homepage version 1.0

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,67 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Form } from "../ui/form"
+import CustomFormField from "../CustomFormField"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+ 
+export enum FormFieldTypes {
+    INPUT = "input",
+    CHECKBOX = "checkbox",
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton',
+}
+
+export function PatientForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    }, 
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+            <h1 className="header">Hi There</h1>
+            <p className="text-dart-700">Schedule your first appointment.</p>
+        </section>
+        
+        <CustomFormField
+            fieldType={FormFieldTypes.INPUT}
+            control={form.control}
+            name="name"
+            label="Full name"
+            placeholder="Nguyen Hoang Long"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default PatientForm
