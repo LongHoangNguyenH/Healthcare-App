3464668909c8b17f22ca170a9d72236c336b2456
LongHoangNguyenH <longnh.uit@gmail.com>
2024-08-28 15:45:26 +0700

    add button from shadcn and change util.ts to utils.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/util";
+import { cn } from "@/lib/utils";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
