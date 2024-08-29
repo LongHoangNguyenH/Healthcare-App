3c1ffcc7b2b5e5147306ca169a11aa05f14cbe97
LongHoangNguyenH <longnh.uit@gmail.com>
2024-08-29 14:15:31 +0700

    information page ver1.0

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,9 +13,10 @@ import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
 import { FormFieldTypes } from "./forms/PatientForm";
 import Image from "next/image";
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
-import {E164Number} from 'libphonenumber-js'
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { E164Number } from "libphonenumber-js";
+import DatePicker from "react-datepicker";
 
 interface CustomProps {
   control: Control<any>;
@@ -33,49 +34,84 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const {
+    fieldType,
+    iconSrc,
+    iconAlt,
+    placeholder,
+    showTimeSelect,
+    dateFormat,
+    renderSkeleton,
+  } = props;
 
-    const {fieldType, iconSrc, iconAlt, placeholder} = props;
-
-    switch (fieldType) {
+  switch (fieldType) {
     case FormFieldTypes.INPUT:
-      return(
+      return (
         <div className="flex rounded-md border border-dark-500 bg-dark-400">
-            {iconSrc && (
-                <Image
-                    src={iconSrc}
-                    alt={iconAlt || 'icon'}
-                    width={24}
-                    height={24}
-                    className="ml-2"
-                />
-            )}
-            <FormControl>
-                <Input
-                    placeholder={placeholder}
-                    {...field}
-                    className="shad-input border-0"
-                />
-            </FormControl>
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || "icon"}
+              width={24}
+              height={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
         </div>
-      )
-    
-    case FormFieldTypes.PHONE_INPUT:
-        return (
-            <FormControl>
-                <PhoneInput
-                    defaultCountry="VN"
-                    placeholder= {placeholder}
-                    international
-                    withCountryCallingCode
-                    value={field.value as E164Number |undefined}
-                    onChange={field.onChange}
-                    className="input-phone"
-                />
-            </FormControl>
-        )
+      );
 
+    case FormFieldTypes.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="VN"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
+      );
+
+    case FormFieldTypes.DATE_PICKER:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-500">
+          <Image
+            src="/assets/icons/calendar.svg"
+            height={24}
+            width={24}
+            alt="calendar"
+            className="ml-2"
+          />
+
+          <FormControl>
+            <DatePicker
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+              dateFormat={dateFormat ?? "MM/dd/yyyy"}
+              showTimeSelect={showTimeSelect ?? false}
+              timeInputLabel="Time:"
+              wrapperClassName="date-picker"
+            />
+          </FormControl>
+        </div>
+      );
+
+    case FormFieldTypes.SKELETON:
+      return (
+        renderSkeleton ? renderSkeleton(field): null
+      )
     default:
-        return null;
+      return null;
   }
 };
 
