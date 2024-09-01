f8d1e89b207ed9aa0c6f1707bfe7568c8f37f3df
LongHoangNguyenH <longnh.uit@gmail.com>
2024-09-01 09:36:56 +0700

    fix bug deployment

--- components/FiledUploader.tsx
+++ components/FiledUploader.tsx
@@ -1,20 +1,22 @@
 "use client";
-import { convertFileToUrl } from "@/lib/utils";
+
 import Image from "next/image";
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+
+import { convertFileToUrl } from "@/lib/utils";
 
 type FileUploaderProps = {
   files: File[] | undefined;
   onChange: (files: File[]) => void;
 };
 
-const FileUploader = ({ files, onChange }: FileUploaderProps) => {
+export const FileUploader = ({ files, onChange }: FileUploaderProps) => {
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
     onChange(acceptedFiles);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div {...getRootProps()} className="file-upload">
@@ -29,30 +31,23 @@ const FileUploader = ({ files, onChange }: FileUploaderProps) => {
         />
       ) : (
         <>
-          <Image 
+          <Image
             src="/assets/icons/upload.svg"
             width={40}
             height={40}
-            alt="=upload"
+            alt="upload"
           />
           <div className="file-upload_label">
-            <p className="text-14-regular">
-              <span className="text-green-500">Click to upload</span>
-              or drag to drop
+            <p className="text-14-regular ">
+              <span className="text-green-500">Click to upload </span>
+              or drag and drop
             </p>
-            <p>
-              SVG, PNG, JPEG, GIF (max 800 x 400)
+            <p className="text-12-regular">
+              SVG, PNG, JPG or GIF (max. 800x400px)
             </p>
           </div>
         </>
       )}
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
     </div>
   );
 };
-
-export default FileUploader;
